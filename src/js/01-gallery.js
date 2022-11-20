cd63faa701a2a046251d7dfb546bd936edc6e217
Marat Baikin <3371335@gmail.com>
2022-11-20 05:38:27 +0200

    Completed Task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,35 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(SimpleLightbox)
+
+const ref = {
+  imgRef: document.querySelector('div.gallery'),
+};
+
+const creatImgList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a></div>`
+  )
+  .join('');
+
+const imgAdd = () => {
+  ref.imgRef.insertAdjacentHTML('beforeend', creatImgList);
+};
+
+imgAdd();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  disableScroll: false,
+  scrollZoom: false,
+});
