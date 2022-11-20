25c700b24905a3b590241564f974dd36573df4ef
Marat Baikin <3371335@gmail.com>
2022-11-20 23:29:07 +0200

    Completed taask 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+console.dir(Player);
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlayerTimeUpdate = function (date) {
+    localStorage.setItem('videoplayer-current-time', date.seconds);
+    
+  }
+player.on('timeupdate', throttle(onPlayerTimeUpdate,1000));
+
+const stopTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(stopTime)
+
 
