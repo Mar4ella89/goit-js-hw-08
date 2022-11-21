aa978acfd001cc2c2e93dfc9701dc2efb77aad03
Marat Baikin <3371335@gmail.com>
2022-11-21 12:12:32 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const loginFofmEl = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+
+const STORAGE_KEY = 'feedback-msg';
+
+const formDate = {};
+
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+const onTextareaInput = event => {
+  const message = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+};
+
+const populateTextarea = () => {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    textarea.value = savedMessage;
+  }
+};
+
+loginFofmEl.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('input', throttle(onTextareaInput, 500));
+loginFofmEl.addEventListener('input', event => {
+  formDate[event.target.name] = event.target.value;
+  console.log(formDate);
+});
+
+populateTextarea();
